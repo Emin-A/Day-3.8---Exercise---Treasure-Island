3e778d87f66508f292a7b2a108a591e092f5e091
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-28 17:37:32 +0000

    Changed the order and structure of the if,elif statement. Does not work!

--- main.py
+++ main.py
@@ -23,25 +23,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island!")
 print("Your mission is to find the hidden treasure!")
-print(input("You are stranded on an island and in front of you there two paths, left and right.\n Which path do you choose?\n"))
+print("You are stranded on an island and in front of you there two paths, left and right.\n Which path do you choose?")
+input()
 
-path_one = "left"
-path_two = "right"
-if input == path_one:
-  print(input("There is pond in your way, do you choose to swim or wait?/n"))
-  
-  path_three = "swim"
-  path_four = "wait"
-  if input == path_three:
-    print(input("You have arrived at a abandoned castle but there two entrance doors./n Which door do you enter, the red one or the green one?/n"))
-    red_door = "red"
-    green_door = "green"
-    if input == red_door:
-      print("You enter a safe haven room full of treasure and food and you live like a KING!/nHail to the king baby!/nTHE END")
-    elif input == green_door:
-    print("You enter the room and there is a portal to another world floating in the air. /n You jump through the portal but land on Teletubbies Island.../nGAME OVER!")
-  elif input == path_four:
-    print("A bunch of cannibals attack you from the bushes and they brutally murder you!/nGAME OVER!")
-elif input == path_two:
-  print("You fell into a pit of snakes, scorpions and crocodiles!/nGAME OVER!")
-  
+# path_one = "left"
+# path_two = "right"
+# path_three = "swim"
+# path_four = "wait"
+# red_door = "red"
+# green_door = "green"
+
+# if input() == path_one:
+#   print("There is pond in your way, do you choose to swim or wait?/n")
+#   input()
+#   if input() == path_three:
+#     input(print("You have arrived at a abandoned castle but there two entrance doors./n Which door do you enter, the red one or the green one?/n"))
+#     if input() == red_door:
+#       input(print("You enter a safe haven room full of treasure and food and you live like a KING!/nHail to the king baby!/nTHE END"))
+#     elif input() == green_door:
+#       print("You enter the room and there is a portal to another world floating in the air. /nYou jump through the portal but land on Teletubbies Island.../nGAME OVER!")
+#   elif input() == path_four:
+#     print("A bunch of cannibals attack you from the bushes and they brutally murder you!/nGAME OVER!")
+# elif input() == path_two:
+#   print("You fell into a pit of snakes, scorpions and crocodiles!/nGAME OVER!")
